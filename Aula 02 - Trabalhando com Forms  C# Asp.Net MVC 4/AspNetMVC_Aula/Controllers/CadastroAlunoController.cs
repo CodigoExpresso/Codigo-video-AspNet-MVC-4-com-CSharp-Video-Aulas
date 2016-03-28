8510009253767e1d519b6cdec13864af728ec67b
Antonio Azevedo <acaz@hotmail.com.br>
2016-03-27 22:35:10 -0300

    Aula 02

---
+++ Aula 02 - Trabalhando com Forms  C# Asp.Net MVC 4/AspNetMVC_Aula/Controllers/CadastroAlunoController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetMVC_Aula.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_Aula.Controllers
+{
+    public class CadastroAlunoController : Controller
+    {
+        public ActionResult CadastroAluno()
+        {
+            ModAluno aluno = new ModAluno();
+            return View(aluno);
+        }
+
+        [HttpPost]
+        public ActionResult ExibeCadastroAluno(FormCollection form)
+        {
+            ViewBag.IdAluno = form["IdAluno"];
+            ViewBag.Nome = form["Nome"];
+            ViewBag.Email = form["Email"];
+
+            return View();
+        }
+    }
+}
