d9397ace4e0aac58514eb3c0107626b11b4bf0f7
Antonio Azevedo <acaz@hotmail.com.br>
2016-04-28 19:10:52 -0300

    Aula 04 (Parte 01)

---
+++ Aula 04 (Parte 01) - Validacao Formularios com DataAnnotation  - Parte 01/AspNetMVC_Aula/Models/ModAluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMVC_Aula.Models
+{
+    public class ModAluno
+    {
+        [Display(Name="Código do Aluno")]
+        [Range(minimum:1, maximum:50, ErrorMessage="Código do Aluno de ser entre 1 e 50.")]
+        public int IdAluno { get; set; }
+
+        [Display(Name = "Nome do Aluno")]
+        [Required(ErrorMessage="Campo Nome deve ser preenchido.")]
+        [StringLength(50, MinimumLength=4, ErrorMessage="Campo Nome deve ter entre 4 e 50 caracters.")]
+        public string Nome { get; set; }
+
+        [Display(Name = "E-mail do Aluno")]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Formato do E-mail Inválido.")]
+        public string Email { get; set; }
+    }
+}
---
+++ Aula 04 (Parte 01) - Validacao Formularios com DataAnnotation  - Parte 01/README.md
@@ -0,0 +1,12 @@
+﻿Código da video-aula AspNet MVC 4 com C#(CSharp)
+---
+Exemplo prático de de como desenvolver sua primeira aplicação em AspNet MVC 4 com C#(Csharp) Visual Studio 2013
+
+## Valicação de formularios com DataAnnotation
+
+###Aula 04 (Parte 01)     
+>Veja a postagem em http://www.codigoexpresso.com.br/Home/Postagem/41      
+>Assista a Vídeo em https://www.youtube.com/watch?v=OGH-jo1XEc4     
+
+
+Visite nossa página www.codigoexpresso.com.br
